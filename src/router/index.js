9b6b989877d3e43205b15b339a31b806f01a8ec1
stellanechi <stellanechi65@gmail.com>
2026-01-15 04:49:55 +0100

    updated route page

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("@/pages/home/HomePage.vue"),
+    },
+    {
+      path: "/auth",
+      name: "auth",
+      component: () => import("@/auth/AuthLayout.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
